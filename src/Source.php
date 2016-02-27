2bd65210c8849732f77f127ffba3535ab5d9399a
Hernán R <hernan@null.net>
2016-02-26 20:34:05 -0430

    Added loadOnce property to Source class

--- src/Source.php
+++ src/Source.php
@@ -10,13 +10,33 @@ class Source
   
   protected $data = null;
   
+  /**
+  * @var boolean Whether the file should be read once or every time getData is called
+  */
+  public $loadOnce;
+  
+  /**
+  * @var string Path to the last file loaded
+  */
+  public $filePath;
+  
+  function __construct($loadOnce = true)
+  {
+    $this->loadOnce = $loadOnce;
+  }
+  
   public function loadFromFile($filePath)
   {
+    $this->filePath = $filePath;
     $this->data = file_get_contents($filePath);
   }
 
   public function getData()
   {
+    if (!$this->loadOnce) {
+      $this->loadFromFile($this->filePath);
+    }
+    
     return $this->data;
   }
   
